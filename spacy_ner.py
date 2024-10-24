0bfd587279a1af751f76e16feeed261e3d9b0fed
JayEpic <jason.yang@epitech.eu>
2024-10-24 14:37:11 +0200

    Learning vocab in Spacy

--- spacy_ner.py
+++ spacy_ner.py
@@ -1,18 +1,8 @@
 import spacy
 
-from spacy.matcher import Matcher
-
 nlp = spacy.load("fr_core_news_sm")
 
-matcher = Matcher(nlp.vocab)
+doc = nlp("Ines nage souvent")
+print("valeur de hash :", nlp.vocab.strings["souvent"])
 
-pattern = [{"TEXT": "iPhone"}, {"TEXT": "12"}]
-matcher.add("IPHONE_PATTERN", [pattern])
-
-doc = nlp("La date de sortie du futur iPhone 12 a fuité")
-
-matches = matcher(doc)
-
-for match_id, start, end in matches:
-    matched_span = doc[start:end]
-    print(matched_span.text)
+print("valeur de chaîne :", nlp.vocab.strings[821433950267086228])
