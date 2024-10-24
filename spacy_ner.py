8e1b121c7388f6f9fd2e30cc40a76e75bc221347
JayEpic <jason.yang@epitech.eu>
2024-10-24 14:40:08 +0200

    Comments added

--- spacy_ner.py
+++ spacy_ner.py
@@ -3,6 +3,6 @@ import spacy
 nlp = spacy.load("fr_core_news_sm")
 
 doc = nlp("Ines nage souvent")
-print("valeur de hash :", nlp.vocab.strings["souvent"])
+print("valeur de hash :", nlp.vocab.strings["souvent"]) # Expose le hash de la chaîne stockée dans Vocab
 
-print("valeur de chaîne :", nlp.vocab.strings[821433950267086228])
+print("valeur de chaîne :", nlp.vocab.strings[821433950267086228]) # Expose la valeur de la chaîne stockée dans Vocab
