7a2dd965cf88e4b10195212663151daeb200ae79
JayEpic <jason.yang@epitech.eu>
2024-10-24 14:52:52 +0200

    Parameters test in spacy with Vocab

--- spacy_ner.py
+++ spacy_ner.py
@@ -3,6 +3,9 @@ import spacy
 nlp = spacy.load("fr_core_news_sm")
 
 doc = nlp("Ines nage souvent")
+lexeme = nlp.vocab["souvent"]
 print("valeur de hash :", nlp.vocab.strings["souvent"]) # Expose le hash de la chaîne stockée dans Vocab
 
 print("valeur de chaîne :", nlp.vocab.strings[821433950267086228]) # Expose la valeur de la chaîne stockée dans Vocab
+
+print(lexeme.text, lexeme.orth, lexeme.is_alpha)
